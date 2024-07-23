f5477f88dfdb5333aeed86241f6ddff34fe8029b
Ryantayy <Ryan.TayPengYeow@lincolnuni.ac.nz>
2024-07-23 18:53:34 +1200

    Setup error pages, update

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,3 +1,5 @@
+import agent from "../../app/api/agent";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 import { Product } from "../../app/models/product";
 import ProductList from "./ProductList";
 import { useState, useEffect } from "react";
@@ -9,13 +11,16 @@ interface Props {
 
 export default function Catalog() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/products")
-      .then((response) => response.json())
-      .then((data) => setProducts(data))
-      .catch((error) => console.error(error));
+    agent.Catalog.list()
+    .then(products => setProducts(products))
+    .catch(error => console.error(error))
+    .finally(() => setLoading(false))
   }, []);
+
+  if (loading) return <LoadingComponent message='Loading products...'></LoadingComponent>
 
   return (
     <>
