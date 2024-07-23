0f8a1d2696a11d6510c060248a196cb0386eb07f
Ryyytay <taypeng1214@gmail.com>
2024-07-23 18:53:34 +1200

    Setup error pages, update

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5075dd959b0fd1ecfb44ebf8df96c6d65aaf80a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b10dedf3c389d1dba13b940bf6f8ebb2ea616c4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
