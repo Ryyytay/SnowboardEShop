30e29b9f0d6260caba8e96939d35c800839e2549
Ryyytay <taypeng1214@gmail.com>
2024-08-03 12:04:43 +1200

    Update

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -11,95 +7,97 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
-namespace API.Controllers
-{   
-    public class AccountController : BaseApiController
+namespace API.Controllers;
+
+public class AccountController : BaseApiController
+{
+    private readonly UserManager<User> _userManager;
+    private readonly TokenService _tokenService;
+    private readonly StoreContext _context;
+
+    public AccountController(UserManager<User> userManager, TokenService tokenService, StoreContext context)
     {
-        private readonly UserManager<User> _userManager;
-        private readonly TokenService _tokenService;
-        private readonly StoreContext _context;
-        public AccountController(UserManager<User> userManager, TokenService tokenService, StoreContext context)
+        _context = context;
+        _tokenService = tokenService;
+        _userManager = userManager;
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+    {
+        var user = await _userManager.FindByNameAsync(loginDto.Username);
+        if (user == null || !await _userManager.CheckPasswordAsync(user, loginDto.Password))
+            return Unauthorized();
+
+        var userBasket = await RetrieveBasket(loginDto.Username);
+        var anonBasket = await RetrieveBasket(Request.Cookies["buyerId"]);
+
+        if (anonBasket != null)
         {
-            _context = context;
-            _tokenService = tokenService;
-            _userManager = userManager;
+            if (userBasket != null) _context.Baskets.Remove(userBasket);
+            anonBasket.BuyerId = user.UserName;
+            Response.Cookies.Delete("buyerId");
+            await _context.SaveChangesAsync();
         }
 
-        [HttpPost("login")]
-        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto) 
+        return new UserDto
         {
-            var user = await _userManager.FindByNameAsync(loginDto.Username);
-            if (user == null || !await _userManager.CheckPasswordAsync(user, loginDto.Password))
-                return Unauthorized();
+            Email = user.Email,
+            Token = await _tokenService.GenerateToken(user),
+            Basket = anonBasket != null ? anonBasket.MapBasketToDto() : userBasket?.MapBasketToDto()
+        };
+    }
 
-            var userBasket = await RetrieveBasket(loginDto.Username);
-            var anonBasket = await RetrieveBasket(Request.Cookies["buyerId"]);
+    [HttpPost("register")]
+    public async Task<ActionResult> RegisterUser(RegisterDto registerDto)
+    {
+        var user = new User { UserName = registerDto.Username, Email = registerDto.Email };
 
-            if (anonBasket != null)
+        var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+        if (!result.Succeeded)
+        {
+            foreach (var error in result.Errors)
             {
-                if (userBasket == null) _context.Baskets.Remove(userBasket);
-                anonBasket.BuyerId = user.UserName;
-                Response.Cookies.Delete("buyerId");
-                await _context.SaveChangesAsync();
+                ModelState.AddModelError(error.Code, error.Description);
             }
 
-            return new UserDto
-            {
-                Email = user.Email,
-                Token = await _tokenService.GenerateToken(user),
-                Basket = anonBasket != null ? anonBasket.MapBasketToDto() : userBasket.MapBasketToDto()
-            };
+            return ValidationProblem();
         }
 
-        [HttpPost("register")]
-        public async Task<ActionResult> Register(RegisterDto registerDto)
+        await _userManager.AddToRoleAsync(user, "Member");
+
+        return StatusCode(201);
+    }
+
+    [Authorize]
+    [HttpGet("currentUser")]
+    public async Task<ActionResult<UserDto>> GetCurrentUser()
+    {
+        var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+        var userBasket = await RetrieveBasket(User.Identity.Name);
+
+        return new UserDto
         {
-            var user = new User{UserName = registerDto.Username, Email = registerDto.Email};
+            Email = user.Email,
+            Token = await _tokenService.GenerateToken(user),
+            Basket = userBasket?.MapBasketToDto()
+        };
+    }
 
-            var result = await _userManager.CreateAsync(user, registerDto.Password);
-
-            if (!result.Succeeded)
-            {
-                foreach (var error in result.Errors) 
-                {
-                    ModelState.AddModelError(error.Code, error.Description);
-                }
-
-                return ValidationProblem();
-            }
-
-            await _userManager.AddToRoleAsync(user, "Member");
-
-            return StatusCode(201);
-        }
-
-        [Authorize]
-        [HttpGet("currentUser")]
-        public async Task<ActionResult<UserDto>> GetCurrentUser()
+    private async Task<Basket> RetrieveBasket(string buyerId)
+    {
+        if (string.IsNullOrEmpty(buyerId))
         {
-            var user = await _userManager.FindByNameAsync(User.Identity.Name);
-
-            return new UserDto
-            {
-                Email = user.Email,
-                Token = await _tokenService.GenerateToken(user)
-            };
+            Response.Cookies.Delete("buyerId");
+            return null;
         }
 
-        private async Task<Basket> RetrieveBasket(string buyerId)
-        {   
-            if (string.IsNullOrEmpty(buyerId))
-            {
-                Response.Cookies.Delete("buyerId");
-                return null;
-            }
-
-            return await _context.Baskets
-                            .Include(i => i.Items)
-                            .ThenInclude(p => p.Product)
-                            .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
-        }
+        return await _context.Baskets
+            .Include(i => i.Items)
+            .ThenInclude(p => p.Product)
+            .FirstOrDefaultAsync(basket => basket.BuyerId == buyerId);
     }
 }
--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -7,7 +7,7 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Paper } from "@mui/material";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { FieldValues, useForm } from "react-hook-form";
 import { LoadingButton } from "@mui/lab";
 import { useDispatch } from "react-redux";
@@ -16,7 +16,8 @@ import { signInUser } from "./accountSlice";
 const defaultTheme = createTheme();
 
 export default function Login() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const location = useLocation();
   const dispatch = useDispatch();
   const {
     register,
@@ -27,8 +28,12 @@ export default function Login() {
   });
 
   async function submitForm(data: FieldValues) {
-    await dispatch(signInUser(data));
-    navigate('/catalog');
+    try {
+      await dispatch(signInUser(data));
+      navigate(location.state?.from || "/catalog");
+    } catch (error: any) {
+      console.log(error);
+    }
   }
 
   return (
