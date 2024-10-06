24ec3dd8f1cd422f24e921308048d97f61177e25
Ryantayy <Ryan.TayPengYeow@lincolnuni.ac.nz>
2024-10-07 10:18:59 +1300

    ADD CRUD

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d42ea3a612b26dd046695bd3a66140346d19a1af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fb2a5002303180391d8ffe65983744714a082a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
